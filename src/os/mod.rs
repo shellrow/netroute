44f2c37324014fd389258d805b15d152863c9cef
shellrow <shellrow@foctal.com>
2025-10-13 18:00:38 +0900

    Support Apple targets

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -2,8 +2,8 @@
 pub(crate) mod unix;
 #[cfg(any(target_os = "linux", target_os = "android"))]
 pub(crate) mod linux;
-#[cfg(target_os = "macos")]
-pub(crate) mod macos;
+#[cfg(target_vendor = "apple")]
+pub(crate) mod darwin;
 #[cfg(any(target_os = "freebsd", target_os = "openbsd", target_os = "netbsd"))]
 pub(crate) mod bsd;
 #[cfg(target_os = "windows")]
@@ -11,8 +11,8 @@ pub(crate) mod windows;
 
 #[cfg(any(target_os = "linux", target_os = "android"))]
 pub(crate) use linux::list_routes_linux as list_routes;
-#[cfg(target_os = "macos")]
-pub(crate) use macos::list_routes_macos as list_routes;
+#[cfg(target_vendor = "apple")]
+pub(crate) use darwin::list_routes_macos as list_routes;
 #[cfg(any(target_os = "freebsd", target_os = "openbsd", target_os = "netbsd"))]
 pub use bsd::list_routes_bsd as list_routes;
 #[cfg(target_os = "windows")]
